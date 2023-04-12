bb9e775fe8e73480b9af255acd4863c722fb69ac
flakytestresearch <128331437+flakytestresearch@users.noreply.github.com>
2023-04-12 01:25:56 -0300

    Update karma.config.js

--- karma.config.js
+++ karma.config.js
@@ -62,7 +62,16 @@ module.exports = function(config) {
         // test results reporter to use
         // possible values: "dots", "progress", "junit", "growl", "coverage"
         reporters: ["coverage", "spec"],
-
+junitReporter: {
+            outputDir: config.outputDir, // results will be saved as $outputDir/$browserName.xml
+            outputFile: config.outputFile, // if included, results will be saved as $outputDir/$browserName/$outputFile
+            suite: "", // suite will become the package name attribute in xml testsuite element
+            useBrowserName: true, // add browser name to report and classes names
+            nameFormatter: undefined, // function (browser, result) to customize the name attribute in xml testcase element
+            classNameFormatter: undefined, // function (browser, result) to customize the classname attribute in xml testcase element
+            properties: {}, // key value pair of properties to add to the <properties> section of the report
+            xmlVersion: null // use '1' if reporting to be per SonarQube 6.2 XML format
+        },
         // web server port
         port: 9876,
 
